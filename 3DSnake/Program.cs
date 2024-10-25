8ba614fcb3ca16db43844d9d3136de0bbc70f6d3
whYXZee <email@email.com>
2024-10-25 09:29:39 -0500

    set up solution & SDL update README add .gitignore

---
+++ 3DSnake/Program.cs
@@ -0,0 +1,7 @@
+﻿using ThreeDSnake.Testing.RuntimeTest;
+
+// See https://aka.ms/new-console-template for more information
+
+// This file doesn't need namespaces nor classes. It just runs everything in the file.
+
+RuntimeTest test = new RuntimeTest(null);
---
+++ 3DSnake/Testing/RuntimeTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace ThreeDSnake.Testing.RuntimeTest
+{
+	public class RuntimeTest
+	{
+		public RuntimeTest(string input)
+		{
+			if (input == null || input == "") {
+				System.Diagnostics.Debug.WriteLine("The code is running, you better catch it!");
+			} else {
+                System.Diagnostics.Debug.WriteLine(input);
+            }
+		}
+	}
+}
