bc1c0793a1f68187b240e4564167c9913aa22df1
whYXZee <email@email.com>
2024-11-14 08:27:45 -0600

    Window, Initializing, trying OpenGL

--- 3D-Snake/src/App.java
+++ 3D-Snake/src/App.java
@@ -1,5 +1,8 @@
+import application.SDL;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // System.out.println("Hello, World!");
+        SDL sdl = new SDL();
     }
 }
