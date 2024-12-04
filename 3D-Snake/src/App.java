c4d39132b1c2f21743bee7b6f945b12bc1281e60
whYXZee <124375418+whYXZee@users.noreply.github.com>
2024-12-04 12:56:42 -0600

    java dev (#1)
    * Window, Initializing, trying OpenGL
    
    * Changed lib to LWJGL, render tests
    
    * Added Snake.java, removed SDL files
    
    ---------
    
    Co-authored-by: whYXZee <email@email.com>

--- 3D-Snake/src/App.java
+++ 3D-Snake/src/App.java
@@ -1,5 +1,8 @@
+import application.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        OpenGL openGL = new OpenGL();
+        openGL.run();
     }
 }
---
+++ 3D-Snake/src/application/AppConstants.java
@@ -0,0 +1,35 @@
+package application;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetEncoder;
+
+public class AppConstants {
+    // public static boolean gameLoop = true;
+    //
+    // IDK
+    //
+    static Charset charset = Charset.forName("UTF-8");
+
+    //
+    // Window
+    //
+    public static Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+    public static int width = (int) screen.getWidth();
+    public static int height = (int) screen.getHeight();
+
+    public static ByteBuffer stringToBB(String input) {
+        CharsetEncoder encoder = charset.newEncoder();
+
+        try {
+            return encoder.encode(CharBuffer.wrap(input));
+        } catch (Exception e) {
+            System.out.println("error");
+        }
+
+        return null;
+    }
+}
