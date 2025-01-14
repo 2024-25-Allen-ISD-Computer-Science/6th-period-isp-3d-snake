d47fb325c65180b7758e0117879741226ec6aa9d
kingstonares <67057610+kingstonares@users.noreply.github.com>
2025-01-14 18:23:07 +0000

    Added movement variables and statements

--- 3D-Snake/src/application/Snake.java
+++ 3D-Snake/src/application/Snake.java
@@ -9,5 +9,27 @@ public class Snake {
     // Movement
     //
 
-    // Kingston: put movement methods and variables here
+    float xPos, yPos, zPos;
+    float speed = 1.0f;
+
+    void key_callback(GLFWindow* window, int key, int scancode, int action, intmods);
+
+    int wState = glfwGetKey(window, GLFW_KEY_W);
+    int aState = glfwGetKey(window, GLFW_KEY_A);
+    int sState = glfwGetKey(window, GLFW_KEY_S);
+    int dState = glfwGetKey(window, GLFW_KEY_D);
+
+    if (key == GLFW_KEY_W && wState = GLFW_PRESS) {
+        turnUp();
+    }
+    else if (key == GLFW_KEY_A && aState = GLFW_PRESS) {
+        turnLeft();
+    }
+    else if (key == GLFW_KEY_S && sState = GLFW_PRESS) {
+        turnDown();
+    }
+    else if (key == GLFW_KEY_D && dState = GLFW_PRESS) {
+        turnRight();
+    }
+
 }
