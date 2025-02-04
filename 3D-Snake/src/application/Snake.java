43f3648172f2d81f9ec2ed89791c39631bf48579
RyanNg590 <ryanng1099@gmail.com>
2025-02-04 18:02:37 +0000

    Udpated Movement

--- 3D-Snake/src/application/Snake.java
+++ 3D-Snake/src/application/Snake.java
@@ -10,4 +10,21 @@ public class Snake {
     //
 
     // Kingston: put movement methods and variables here
+
+    public void changeDirection(Vector3f newDir) {
+    if (!newDir.equals(direction.negate(new Vector3f()))) direction.set(newDir);
+}
+
+public void move() {
+    
+    if (segments.isEmpty()) return;
+    Vector3f prev = new Vector3f(segments.get(0).getPosition());
+    segments.get(0).incPosition(direction.x, direction.y, direction.z);
+   
+    for (int i = 1; i < segments.size(); i++) {
+        Vector3f curr = new Vector3f(segments.get(i).getPosition());
+        segments.get(i).setPosition(prev);
+        prev.set(curr);
+    }
+}
 }
