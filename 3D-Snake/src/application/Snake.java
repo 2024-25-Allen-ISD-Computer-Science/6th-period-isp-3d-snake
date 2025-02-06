d48949e1858578cf1e677c9526f6b1712725f30c
kingstonares <67057610+kingstonares@users.noreply.github.com>
2025-02-06 03:55:54 +0000

    wrote methods for turning and declared + instantiated position and direction vectors for snake

--- 3D-Snake/src/application/Snake.java
+++ 3D-Snake/src/application/Snake.java
@@ -1,18 +1,23 @@
 package application;
 
+import org.joml.Vector3f;
 import org.lwjgl.glfw.GLFW;
 
 public class Snake {
-    public Snake() {
+    private Vector3f position;
+    private Vector3f direction;
+    private float speed = 1.0f;
 
+    public Snake() {
+        position = new Vector3f(0, 0, 0);
+        direction = new Vector3f(0, 0, -1);
     }
 
     //
     // Movement
     //
 
-    float xPos, yPos, zPos;
-    float speed = 1.0f;
+    
 
     public void keyCallback(long window, int key, int scancode, int action, int mods) {
 
@@ -30,5 +35,31 @@ public class Snake {
 
     }
 
-    public void 
+    public void turnUp() {
+        if (direction.z != 0 || direction.x != 0) {
+            direction.set(0, 1, 0);
+        }
+    }
+
+    public void turnDown() {
+        if (direction.z != 0 || direction.x != 0) {
+            direction.set(0, -1, 0);
+        }
+    }
+
+    public void turnLeft() {
+        if (direction.x != 0) {
+            direction.set(0, 0, -direction.x);
+        } else if (direction.z != 0) {
+            direction.set(-direction.z, 0, 0);
+        }
+    }
+
+    public void turnRight() {
+        if (direction.x != 0) {
+            direction.set(0, 0, direction.x);
+        } else if (direction.z != 0) {
+            direction.set(direction.z, 0, 0);
+        }
+    }
 }
